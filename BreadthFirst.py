74159f3cb1461834e480e261410f95557ee7e2fa
samuel <samuellpvt@gmail.com>
2023-05-03 21:32:37 -0300

    test

--- BreadthFirst.py
+++ BreadthFirst.py
@@ -1,11 +1,45 @@
 from collections import deque
-
+from copy import deepcopy
 class breadthFirst():
     
-    def __init__(self, dict, start,end):
-        self.graph =dict
+    def __init__(self, start,end,size):
+        # self.graph =graph
         self.start =start
         self.end =end
+        self.graph = {}
+        self.generatePermutationsList(size)
+        self.populateAdjList(start)
+
+        print(self.permList)
+
+    def generatePermutationsList(self, size):
+        self.permList = []
+        for i in range(size):
+            for j in range(i+1, size):
+                self.permList.append((i, j, 2 if j-i == 1 else 4))
+
+    # Use the swap list to generate all possible permutations of any list and populates the Adj list
+    def populateAdjList(self, list):
+        if list not in self.graph.keys():
+            self.graph[list] = {}
+        for i in self.permList:
+                    a,b,c = i
+                    split_list = list.split(',')
+                    if split_list[b] ==split_list[b]:
+                        swap_list = self.swap(split_list, a, b)
+                        swap_string_list = ','.join(str(x) for x in swap_list)
+                        self.graph[list][swap_string_list]=c
+                   
+        # simple swap function
+    def swap(self, list, a, b):
+        # needs to copy bc python always pass immutable objs as refence
+        list = deepcopy(list)
+        if  1==1:
+            temp = list[a]
+            list[a] = list[b]
+            list[b] = temp
+            return list
+        return list
 
     def getPath(self):
         queue = deque([self.start])
@@ -14,7 +48,7 @@ class breadthFirst():
         while len(queue)>0:
         
             v = queue.popleft()
-           
+            self.populateAdjList(v)
             if v == self.end:
                 path = []
                 while v is not None:
@@ -26,13 +60,17 @@ class breadthFirst():
                 return
         
             for i in self.graph[v].keys():
+
                 neighbor = i
                 if neighbor not in visited:
+
                     visited[neighbor] = v
                     queue.append(neighbor)
+        
 
     
     def printPath(self, printPath):
+        # print(self)
         print(self.size ,len(self.path)-1)
         if printPath:
             for i in self.path:
--- TP1.py
+++ TP1.py
@@ -34,14 +34,17 @@ class adjacencyDict():
                     swap_string_list = ','.join(str(x) for x in swap_list)
                     self.dict[poped_ele][swap_string_list]=c
                     stack.append(swap_string_list)
+        
 
     # simple swap function
     def swap(self, list, a, b):
         # needs to copy bc python always pass immutable objs as refence
         list = deepcopy(list)
-        temp = list[a]
-        list[a] = list[b]
-        list[b] = temp
+        if  1==1:
+            temp = list[a]
+            list[a] = list[b]
+            list[b] = temp
+            return list
         return list
 
 
@@ -54,25 +57,25 @@ def main():
     for i in range(3,int(size)+3):
         start.append(sys.argv[i])
     
-    adjList = adjacencyDict( start)
-    adjList.generatePermutationsList(size)
-    adjList.populateAdjList(start)
+    # adjList = adjacencyDict( start)
+    # adjList.generatePermutationsList(size)
+    # adjList.populateAdjList(start)
 
     if algType=="B":
         start_ = time.time()
-        obj = breadthFirst(adjList.dict, ','.join(str(x) for x in start), ','.join(str(x) for x in sorted(start)))
+        obj = breadthFirst(','.join(str(x) for x in start), ','.join(str(x) for x in sorted(start)),len(start))
         obj.getPath()
         obj.printPath( sys.argv[-1] =="PRINT")
         end_ = time.time()
         print(end_ - start_)
     
-    if algType=="I":
-        start_ = time.time()
-        obj = iterativeDeepening(adjList.dict, ','.join(str(x) for x in start), ','.join(str(x) for x in sorted(start)),size)
-        obj.getPath()
-        obj.printPath( sys.argv[-1] =="PRINT")
-        end_ = time.time()
-        print(end_ - start_)
+    # if algType=="I":
+    #     start_ = time.time()
+    #     obj = iterativeDeepening(adjList.dict, ','.join(str(x) for x in start), ','.join(str(x) for x in sorted(start)),size)
+    #     obj.getPath()
+    #     obj.printPath( sys.argv[-1] =="PRINT")
+    #     end_ = time.time()
+    #     print(end_ - start_)
 
 
 main()
